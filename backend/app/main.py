300edf1ebb9124ae06a023067432286c1ee14f8e
Mansi Vaghasiya <vaghasiyamansi999@gmail.com>
2026-02-12 22:28:00 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:3000",
-        "http://localhost:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
